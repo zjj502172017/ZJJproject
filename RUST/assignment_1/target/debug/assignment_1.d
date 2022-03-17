4e6d08a5eb2a5e773a0a6f5c814ea165878ffc32
zjj502172017 <502172017@qq.com>
2022-03-17 10:19:13 +0800

    all assignment_1 update

---
+++ RUST/assignment_1/target/debug/assignment_1.d
@@ -0,0 +1,1 @@
+C:\Users\zjj\Desktop\vscode-environment\RUST\assignment_1\target\debug\assignment_1.exe: C:\Users\zjj\Desktop\vscode-environment\RUST\assignment_1\src\main.rs
---
+++ RUST/assignment_1/target/debug/deps/assignment_1.d
@@ -0,0 +1,5 @@
+C:\Users\zjj\Desktop\vscode-environment\RUST\assignment_1\target\debug\deps\assignment_1.exe: src\main.rs
+
+C:\Users\zjj\Desktop\vscode-environment\RUST\assignment_1\target\debug\deps\assignment_1.d: src\main.rs
+
+src\main.rs:
